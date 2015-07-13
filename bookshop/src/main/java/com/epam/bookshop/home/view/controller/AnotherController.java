8cb1303cd0f423b2709dc36c4d3f6c6e8b17a91d
imreszanyi <imre.szanyi@gmail.com>
2015-07-13 21:46:32 +0200

    c

--- bookshop/src/main/java/com/epam/bookshop/home/view/controller/AnotherController.java
+++ bookshop/src/main/java/com/epam/bookshop/home/view/controller/AnotherController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AnotherController {
@@ -17,5 +18,6 @@ public class AnotherController {
 	public String second(Model model) {
 		model.addAttribute("alma", "rohadt");
 		return "homepage";
+		//return "forward:/shopping";
 	}
 }
